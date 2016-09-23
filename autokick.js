da2f1e78cd2e479c9efb2e79e88fabc1348ec0c2
Blinils <nils.brisset@laposte.net>
2016-09-23 15:40:55 +0200

    Add "autokick" snippet via upload

---
+++ autokick.js
@@ -0,0 +1,6 @@
+whitelist = ["twitter:2612276544", "ID"] //add or remove id players
+channel.socket.on("addActor", function (e)
+{
+	if(whitelist.indexOf(e.authId) == -1)
+		channel.socket.emit("banUser",{authId:e.authId, displayName:e.displayName})
+});
