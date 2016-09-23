65a0f73745b657b448020517d114e69212f2118d
Blinils <nils.brisset@laposte.net>
2016-09-23 15:54:43 +0200

    Update autokick.js (auto unban)

--- autokick.js
+++ autokick.js
@@ -2,5 +2,7 @@ whitelist = ["twitter:2612276544", "ID"] //add or remove id players
 channel.socket.on("addActor", function (e)
 {
 	if(whitelist.indexOf(e.authId) == -1)
-		channel.socket.emit("banUser",{authId:e.authId, displayName:e.displayName})
+		channel.socket.emit("banUser", {authId:e.authId, displayName:e.displayName});
+		setTimeout(function(){}, 2000);
+		channel.socket.emit("unbanUser", e.authId);
 });
