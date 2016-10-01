feb8410404517476ca358589d8bacf06c9de717a
Blinils <nils.brisset@laposte.net>
2016-10-01 20:57:06 +0200

    Add "last_letters.js" via upload

---
+++ last_letters.js
@@ -0,0 +1,14 @@
+function isAlive(element) {
+  return element.state == "alive";
+}
+
+var lastletters = "";
+var usersBP = channel.data.actors.filter(isAlive);
+for(i in usersBP)
+{
+	lastletters += usersBP[i].displayName;
+	lastletters += " : " + usersBP[i].lockedLetters.toString().toUpperCase();
+	lastletters += "\r\n"; 
+}
+
+console.log(lastletters);
